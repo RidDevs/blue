8b7fb08692f08d62252455b4c00bbad27962a24e
Your Name <your.email@example.com>
2025-09-12 10:23:57 +0530

    vite config update, wifi t sole etia

--- satelite/vite.config.js
+++ satelite/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // you can change port if needed
-    open: true  // auto open browser
+    host: '0.0.0.0', // 👈 listen on all network interfaces
+    port: 5173,      // you can change port if needed
+    open: true       // auto open browser on your machine
   }
 })
